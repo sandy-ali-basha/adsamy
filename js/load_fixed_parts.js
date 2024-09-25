d687b38d4600f75b6f0ed10676b42e7f916c5839
sandy <sandy.ali.basha@gmail.com>
2024-09-25 15:51:11 +0300

    new updates

---
+++ js/load_fixed_parts.js
@@ -0,0 +1,14 @@
+// Dynamic Load Fixed Parts
+function includeHTML(url, targetId) {
+	$("#"+targetId).load(url);
+}
+
+// $(document).ready(()=> {
+// 	includeHTML('navbar.html', 'navbar_container');
+// 	includeHTML('footer.html', 'footer_container');
+// });
+
+$( window ).on( "load", function() {
+  includeHTML('navbar.html', 'navbar_container');
+  includeHTML('footer.html', 'footer_container');
+} );
